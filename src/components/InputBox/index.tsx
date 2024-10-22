deb5ceeb53ee5a881645a6d2af96753902de7fee
whgurrb <hgcho3@naver.com>
2024-10-22 16:54:25 +0900

    10-20 commit

--- src/components/InputBox/index.tsx
+++ src/components/InputBox/index.tsx
@@ -11,7 +11,7 @@ const InputBox = forwardRef<HTMLInputElement, Props>( ( props: Props, ref )=>{
             <div className='input-box-title'>{'아이디'}</div>
             <div className='input-box-content'>
                 <div className='input-box-body'>
-                    <input className='input-box-input'></input>
+                    <input className='input-box-input' placeholder={'아이디를 입력하세요'}></input>
                     <div className='input-box-button'>{ '중복 확인' }</div>
                 </div>
                 <div className='input-box-message'>{'사용 가능한 아이디 입니다'}</div>
