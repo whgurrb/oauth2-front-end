09c27ba1ca8195c176a3d80daf2f756495c2def1
whgurrb <hgcho3@naver.com>
2024-10-25 14:29:07 +0900

    Front-end 10-25-start Commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import InputBox from 'components/InputBox';
 import './App.css';
 import { ChangeEvent, useState } from 'react';
 
+
 function App() {
   const onNaverClickHandler=()=>{
     alert('Naver access....');
